567019b1220a983f3e4f38fd57db88397420de63
Anton Sidarau <anton.sidarau@gmail.com>
2016-10-27 17:44:14 +0300

    changed query

--- app/src/main/java/com/example/asus/calculator/tools/loader/CategoryLoadTask.java
+++ app/src/main/java/com/example/asus/calculator/tools/loader/CategoryLoadTask.java
@@ -5,11 +5,10 @@ import android.util.Log;
 import android.view.View;
 import android.widget.ProgressBar;
 
+import com.example.asus.calculator.dao.CategoryDao;
 import com.example.asus.calculator.model.persistent.Category;
 import com.example.asus.calculator.tools.db.DBHelperFactory;
 import com.example.asus.calculator.util.MagicConstants;
-import com.j256.ormlite.dao.Dao;
-import com.j256.ormlite.stmt.QueryBuilder;
 
 import java.sql.SQLException;
 import java.util.List;
@@ -48,13 +47,12 @@ public class CategoryLoadTask extends AsyncTask<Void, Void, List<Category>> {
 
         List<Category> list = null;
         try {
-            Dao<Category, Long> dao = DBHelperFactory.getHelper().getCategoryDao();
-            QueryBuilder<Category, Long> queryBuilder = dao.queryBuilder();
-            queryBuilder.limit(COUNT).offset(startIndex);
-            list = dao.query(queryBuilder.prepare());
+            CategoryDao dao = DBHelperFactory.getHelper().getCategoryDao();
+            list = dao.getLimitedWithOffset(COUNT, startIndex);
         } catch (SQLException e) {
             Log.e(MagicConstants.LOG_TAG, "Loading categories due to onScroll");
         }
+
         return list;
     }
 
